d409fedbb2b31c573ee21a606868eb82e6e85a7f
Stefan Vandermeulen <stefan@netdust.be>
2025-11-06 20:31:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,49 @@
 import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.resolve(process.cwd(), `.env.local`) });
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+await (await import('dotenv')).default.config({
+    path: path.resolve(process.cwd(), '.env.local')
+})
 
 export default defineConfig(({ command }) => ({
     root: process.cwd(), // ✅ WSL-friendly root
-    base: command === 'serve' ? '' :  process.env.VITE_THEME ? `/content/themes/${process.env.VITE_THEME}/assets/dist/` : '/dist/',
+    base: command === 'serve' ? '' :  process.env.VITE_THEME ? `/app/themes/${process.env.VITE_THEME}/assets/dist/` : '/dist/',
     publicDir: false,
+    define: {
+        VERSION: JSON.stringify('3.24.2'),
+        LOG: 'false',
+    },
     build: {
         assetsDir: '',
         emptyOutDir: true,
         manifest: true,
-        outDir: `./app/content/themes/${process.env.VITE_THEME}/assets/dist/`,
+        outDir: `./web/app/themes/${process.env.VITE_THEME}/assets/dist/`,
         rollupOptions: {
             input: {
-                main: path.resolve(process.cwd(), process.env.VITE_ENTRY_POINT || 'src/main.js'),
-            }
+                'uikit-custom': path.resolve(process.cwd(), 'src/uikit-custom.js'),
+                'theme-services': path.resolve(process.cwd(), 'src/theme-services.js'),
+                'ntdst-custom': path.resolve(process.cwd(), 'src/ntdst-custom.js'),
+            },
+            output: {
+                format: 'es',
+                entryFileNames: '[name].js',
+                chunkFileNames: 'chunks/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]'
+            },
         },
+    },
+    resolve: {
+        alias: {
+            '@': process.cwd(),
+            'uikit': path.resolve(__dirname, 'node_modules/uikit/src/js'),
+            'uikit-util': path.resolve(__dirname, 'node_modules/uikit/src/js/util'),
+        },
+        dedupe: ['uikit'],
     },
     server: {
         host: process.env.VITE_HOST || 'localhost',
